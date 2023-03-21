b9e6eb1be334b682464e433ccfa4b6af341f2f19
abdulhameedshaheen <62672461+abdulhameedshaheen@users.noreply.github.com>
2023-03-21 08:47:53 +0300

    iniitial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+# Create Website Onject
+app = Flask(__name__)
+
+
+# connect HTML with the website obj
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__name__":
+    app.run(debug=True)
